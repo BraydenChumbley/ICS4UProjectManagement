e155fd86d7359d26e32a11d7eea4c4c5dcd472a1
Brayden Chumbley <bchumbley2001@gmail.com>
2019-05-13 12:58:42 -0400

    Added an AbstractJSONReader class and a test questions json file

---
+++ SDLC Tester/src/sdlc/tester/AbstractJSONReader.java
@@ -0,0 +1,50 @@
+package sdlc.tester;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author Brayden Chumbley
+ */
+public class AbstractJSONReader {
+    
+    protected JSONParser parser;
+    protected FileReader jsonFileReader;
+    
+    /**
+     * Default constructor
+     * @param jsonFile 
+     */
+    public AbstractJSONReader(File jsonFile)
+    {
+	parser = new JSONParser();
+	try 
+	{
+	    jsonFileReader = new FileReader(jsonFile);
+	} 
+	catch (FileNotFoundException ex) 
+	{
+	    Logger.getLogger(AbstractJSONReader.class.getName()).log(Level.SEVERE, null, ex);
+	}
+    }
+    
+    public AbstractJSONReader(String jsonFilePath)
+    {
+	this(new File(jsonFilePath));
+    }
+    
+    public JSONObject readFile() throws IOException, ParseException, ClassCastException
+    {
+	Object obj = parser.parse(jsonFileReader);
+	return (JSONObject) obj;
+    }
+    
+}
--- SDLC Tester/src/sdlc/tester/SDLCTester.java
+++ SDLC Tester/src/sdlc/tester/SDLCTester.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * YESSS
-NOOOOoo
- */
 package sdlc.tester;
 
 /**
@@ -15,8 +9,9 @@ public class SDLCTester {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-	// TODO code application logic here
+    public static void main(String[] args) 
+    {
+	
     }
     
 }
